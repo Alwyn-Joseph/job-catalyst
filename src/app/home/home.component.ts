4fab1fb8680efd5f5fa6249de4e8591582870f7a
Alwyn Joseph <74452001+Alwyn-Joseph@users.noreply.github.com>
2023-04-25 21:41:31 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  jobsList = [
+    {
+      "title": "Java Developer",
+      "company": "Tata Consultancy Services",
+      "LPA": "8.5",
+      "location": "Bangalore",
+      "type": "Full-Time",
+      "applyURL": "https://currency-converter-aj.web.app/currency-converter",
+      "postDate": "11 Aug, 2023",
+      "deadLine":"11 Sep, 2023",
+      "requirment": {
+        "education": "BE/BTech/MCA/MTech",
+        "passoutYear": "2021/22/23",
+        "experience": "Freshers/1-2 Years",
+        "skillsM": "Java,Spring,Microservices,ORM",
+        "skillsO": "Agile,Angular,React,SQL",
+        "desc": "For this job, the ideal candidate should possess strong proficiency in Java, Spring, Hibernate, and Struts framework, as well as familiarity with SQL queries and Unix commands. The ability to create Junit test cases and experience with all phases of SDLC - Analysis, Design, Coding, Testing, and Deployment, is also a must-have. Additionally, the candidate should be capable of making medium to high complexity code changes and possess strong communication skills."
+      }
+    },
+    {
+      "title": "Java Developer",
+      "company": "Tata Consultancy Services",
+      "LPA": "8 Lakh",
+      "location": "Bangalore",
+      "type": "Full-Time",
+      "applyURL": "https://currency-converter-aj.web.app/currency-converter",
+      "requirment": {
+        "education": "BE/BTech/MCA/MTech",
+        "passoutYear": "2021/22/23",
+        "experience": "Freshers/1-2 Years",
+        "skillsM": "Java,Spring,Microservices,ORM",
+        "skillsO": "Agile,Angular,React,SQL",
+        "desc": "For this job, the ideal candidate should possess strong proficiency in Java, Spring, Hibernate, and Struts framework, as well as familiarity with SQL queries and Unix commands. The ability to create Junit test cases and experience with all phases of SDLC - Analysis, Design, Coding, Testing, and Deployment, is also a must-have. Additionally, the candidate should be capable of making medium to high complexity code changes and possess strong communication skills."
+      }
+    },
+    {
+      "title": "Java Developer",
+      "company": "Tata Consultancy Services",
+      "LPA": "8 Lakh",
+      "location": "Bangalore",
+      "type": "Full-Time",
+      "applyURL": "https://currency-converter-aj.web.app/currency-converter",
+      "requirment": {
+        "education": "BE/BTech/MCA/MTech",
+        "passoutYear": "2021/22/23",
+        "experience": "Freshers/1-2 Years",
+        "skillsM": "Java,Spring,Microservices,ORM",
+        "skillsO": "Agile,Angular,React,SQL",
+        "desc": "For this job, the ideal candidate should possess strong proficiency in Java, Spring, Hibernate, and Struts framework, as well as familiarity with SQL queries and Unix commands. The ability to create Junit test cases and experience with all phases of SDLC - Analysis, Design, Coding, Testing, and Deployment, is also a must-have. Additionally, the candidate should be capable of making medium to high complexity code changes and possess strong communication skills."
+      }
+    },
+    {
+      "title": "Java Developer",
+      "company": "Tata Consultancy Services",
+      "LPA": "8 Lakh",
+      "location": "Bangalore",
+      "type": "Full-Time",
+      "applyURL": "https://currency-converter-aj.web.app/currency-converter",
+      "requirment": {
+        "education": "BE/BTech/MCA/MTech",
+        "passoutYear": "2021/22/23",
+        "experience": "Freshers/1-2 Years",
+        "skillsM": "Java,Spring,Microservices,ORM",
+        "skillsO": "Agile,Angular,React,SQL",
+        "desc": "For this job, the ideal candidate should possess strong proficiency in Java, Spring, Hibernate, and Struts framework, as well as familiarity with SQL queries and Unix commands. The ability to create Junit test cases and experience with all phases of SDLC - Analysis, Design, Coding, Testing, and Deployment, is also a must-have. Additionally, the candidate should be capable of making medium to high complexity code changes and possess strong communication skills."
+      }
+    },
+    {
+      "title": "Java Developer",
+      "company": "Tata Consultancy Services",
+      "LPA": "8 Lakh",
+      "location": "Bangalore",
+      "type": "Full-Time",
+      "applyURL": "https://currency-converter-aj.web.app/currency-converter",
+      "requirment": {
+        "education": "BE/BTech/MCA/MTech",
+        "passoutYear": "2021/22/23",
+        "experience": "Freshers/1-2 Years",
+        "skillsM": "Java,Spring,Microservices,ORM",
+        "skillsO": "Agile,Angular,React,SQL",
+        "desc": "For this job, the ideal candidate should possess strong proficiency in Java, Spring, Hibernate, and Struts framework, as well as familiarity with SQL queries and Unix commands. The ability to create Junit test cases and experience with all phases of SDLC - Analysis, Design, Coding, Testing, and Deployment, is also a must-have. Additionally, the candidate should be capable of making medium to high complexity code changes and possess strong communication skills."
+      }
+    }
+  ]
+
+}
